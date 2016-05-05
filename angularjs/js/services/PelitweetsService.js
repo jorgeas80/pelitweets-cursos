b700ed7051a16a71b2da4280605f29c8006469a4
Jorge Arevalo <jorgeas80@gmail.com>
2016-05-05 18:51:29 +0200

    Added another way to do things in angularjs version, commented

--- angularjs/js/services/PelitweetsService.js
+++ angularjs/js/services/PelitweetsService.js
@@ -4,6 +4,9 @@ function pelitweetsFactory($resource, $http, pelitweetsUrl, pelitweetsBaseMovieU
     f.getMovieList = function() {
         // This returns a promise!
         return $http.get(pelitweetsUrl);
+
+        // another way, NOT returning a promise. It returns the json object
+        //return $resource(pelitweetsUrl).query();
     }
 
     f.getMovieDetail = function(id) {
